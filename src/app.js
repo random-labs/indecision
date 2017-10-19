1893fbd21699d78c2ade99b94653bc1a904c0792
Alexandre Karpov <alexakapov@gmail.com>
2017-10-18 22:13:19 -0400

    data binding, finally

--- src/app.js
+++ src/app.js
@@ -3,10 +3,49 @@
 console.log("app.js is running");
 
 // JSX - JavaScript XML
-var template = <h1>This is a JSX template in src/app.js - very cool</h1>;
+
+var app = {
+    title: "A Title!",
+    subtitle: "some subtitle",
+    options: []
+};
 
 var appRoot = document.getElementById('root');
 
-console.log(appRoot);
+const foo = () => "foo";
 
-ReactDOM.render(template, appRoot);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const option = e.target.optionText.value;
+    if (option) {
+        console.log("Option", option, "submitted");
+        app.options.push(option);
+        e.target.optionText.value='';
+        renderFunc();
+    }
+};
+      
+const renderFunc = () => {
+    const template = (
+	<div>
+	  <h1>{app.title}</h1>
+	  <h2>{app.subtitle ? "Subtitle: " + app.subtitle : "none"}</h2>
+	  <h2>{app.options.length > 0 ? app.options.join(' ') : 'no options'}</h2>
+          <button name='reset' onClick={reset} >Reset options</button>
+          <form onSubmit={onFormSubmit}>
+            <input type="text" name="optionText"/>
+            <button>add option</button>
+          </form>
+        </div>
+    );
+
+    ReactDOM.render(template, appRoot)
+ };
+
+const reset = () => {
+    app.options = [];
+    renderFunc();
+}
+
+renderFunc();
--- src/playground/arrow.js
+++ src/playground/arrow.js
@@ -7,4 +7,10 @@ const squareArrow = (X) => X*X;
 const getFirstName = (FullName) =>
       FullName.split(' ')[0];
 
-//const 
+const multiplier = {
+    numbers: [1,2,3,4,5],
+    multiplyBy: [42],
+    multiply() {
+        return this.numbers.map((number) => number * this.multiplyBy);
+    }
+};
