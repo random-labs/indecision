08be8a09d101d283b82709df9062d31bdeafd0bb
Alexandre Karpov <alexakapov@gmail.com>
2017-10-19 22:27:09 -0400

    components are pretty cool

--- src/app.js
+++ src/app.js
@@ -1,61 +1,51 @@
-"use strict";
-
-console.log("app.js is running");
-
-// JSX - JavaScript XML
-
-var app = {
-    title: "A Title!",
-    subtitle: "some subtitle",
-    options: []
-};
-
-var appRoot = document.getElementById('root');
-
-const foo = () => "foo";
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.optionText.value;
-    if (option) {
-        console.log("Option", option, "submitted");
-        app.options.push(option);
-        e.target.optionText.value='';
-        renderFunc();
-    }
-};
-
-const makeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    console.log(randomNum);
-    const option = app.options[option];
-};
-
-const renderFunc = () => {
-    const template = (
-	<div>
-	  <h1>{app.title}</h1>
-	  <h2>{app.subtitle ? "Subtitle: " + app.subtitle : "none"}</h2>
-          <ol>
-            {app.options.length > 0 ? app.options.map((o) => <li key={o}>{o}</li>): "no options"}
-        </ol>
-
-            <button disabled={app.options.length == 0} name='makeDecision' onClick={makeDecision}>Make Decision</button><br/>
-            <button name='reset' onClick={reset} >Reset options</button>
-            <form onSubmit={onFormSubmit}>
-            <input type="text" name="optionText"/>
-            <button>add option</button>
-            </form>
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+              <h1>Indecision</h1>
+              <h2>Put your life in the hands of a computer</h2>
             </div>
-    );
-
-    ReactDOM.render(template, appRoot);
- };
-
-const reset = () => {
-    app.options = [];
-    renderFunc();
+        );
+    }
 }
 
-renderFunc();
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+              <button>What should I do?</button>
+            </div>
+        );
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+              <p>This is going to be a list of Options</p>
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+              <p>A form for adding an option will live here</p>
+            </div>
+        );
+    }
+}
+
+const jsx = (
+    <div>
+      <Header/>
+      <Action/>
+      <AddOption/>
+      <Options/>
+    </div>
+);
+
+ReactDOM.render(jsx, document.getElementById('root'));
