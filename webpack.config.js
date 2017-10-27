89b8ca167be0a20a883a58f088366f95a92d5c1f
Alexandre Karpov <alexakarpov@gmail.com>
2017-10-27 01:45:35 -0400

    big reset to lecture code r-07-08

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,16 @@
 const path = require('path');
-const fullPath = path.join(__dirname, 'public');
-
-console.log("Full path is ", fullPath);
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: fullPath,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
-    }
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  }
 };
