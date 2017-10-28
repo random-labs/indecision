ec3b89ffd3afe06a1de4b128b122a7c1f96ef607
Alexandre Karpov <alexakarpov@gmail.com>
2017-10-27 21:15:42 -0400

    ckeanup app

--- src/app.js
+++ src/app.js
@@ -2,22 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-class OldS {
-  constructor() {
-    this.name = "Alex";
-  }
-}
-const oldS = new OldS();
-console.log(oldS);
-
-// ---------
-
-class NewS {
-  name = "Jen";
-}
-
-const newS = new NewS();
-
-console.log(newS);
+ReactDOM.render(<IndecisionApp/>, document.getElementById('root'));
