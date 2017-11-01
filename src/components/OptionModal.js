16354b9962c7b30b577b9e5bf766730d4004f35d
Alexandre Karpov <alexakarpov@gmail.com>
2017-11-01 01:27:37 -0400

    styling the 3rd party modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const OptionModal = (props) => (
-    <Modal
+    <Modal isOpen={!!props.selectedOption}
         isOpen={!!props.selectedOption}
         contentLabel="Selected Option"
         onRequestClose={props.handleOK}
+        closeTimeoutMS={400}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleOK}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleOK}>Okay</button>
     </Modal>
         );
 
